9f953833093608e205bf95abe60c86d533f386a6
User <User@DESKTOP-IJBJHMS>
2021-06-17 17:18:25 -0700

    Add project files.

---
+++ GameOn3x/Main.cpp
@@ -0,0 +1,15 @@
+/* File Creator: Paul Boyer;
+* pauboyer@uat.edu
+CSC215 wk 1 */
+//This include lets output to screen.
+#include <iostream>
+// Right now this keeps me from typing std in front of many lines of code.
+using namespace std;
+// This is the entry point function for C++
+int main()
+{
+	// This prints out to the display
+	cout << "Game On!" << endl;
+	// This ends our program
+	return 0;
+}
